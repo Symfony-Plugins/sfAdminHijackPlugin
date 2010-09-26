b72c551b1d25b451b771e33d9f67f604313ca36b
caefer <caefer@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-09-26 13:56:43 -0500

    first release copied from GitHub
    git-svn-id: http://svn.symfony-project.com/plugins/sfAdminHijackPlugin/trunk@30994 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ config/sfAdminHijackPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfAdminHijackPlugin configuration.
+ * 
+ * @package     sfAdminHijackPlugin
+ * @subpackage  config
+ * @author      Christian Schaefer <caefer@ical.ly>
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class sfAdminHijackPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
